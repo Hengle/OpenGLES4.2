380bce9fc99d42c42ce3077a305dff6073ea9574
Chenshiwen <cswzmg@live.com>
2017-05-17 18:16:48 +0800

    Initial Commit

---
+++ OpenGLES-4.2把灯光烘焙进纹理中/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGLES-4.2把灯光烘焙进纹理中
+//
+//  Created by ShiWen on 2017/5/17.
+//  Copyright © 2017年 ShiWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGLES-4.2把灯光烘焙进纹理中/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGLES-4.2把灯光烘焙进纹理中
+//
+//  Created by ShiWen on 2017/5/17.
+//  Copyright © 2017年 ShiWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
